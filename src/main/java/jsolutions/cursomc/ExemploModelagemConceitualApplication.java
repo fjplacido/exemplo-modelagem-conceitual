202cb81070624d5ed6546301154b3fd69c052ee2
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2023-09-16 15:18:18 -0300

    Projeto criado.

---
+++ src/main/java/jsolutions/cursomc/ExemploModelagemConceitualApplication.java
@@ -0,0 +1,13 @@
+package jsolutions.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploModelagemConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploModelagemConceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jsolutions/cursomc/ExemploModelagemConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package jsolutions.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploModelagemConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
